8434553533f900d0e372268a6eefa8fcc441f44b
MMMonir <helpinfosdesk@gmail.com>
2021-11-02 13:56:59 +0600

    Access Token Completed

--- React/src/components/Orders/Orders.js
+++ React/src/components/Orders/Orders.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
+import { useHistory } from 'react-router';
 import useAuth from './../../hooks/useAuth';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const {user} = useAuth();
-
+    const history = useHistory();
     useEffect( () => {
         const url = `http://localhost:5000/orders?email=${user.email}`;
         fetch(url, {headers: {
             'authorization': `Bearer ${localStorage.getItem('idToken')}`
         }})
-        .then(res => res.json())
+        .then(res => {
+            if(res.status === 200){
+                return res.json();
+            }
+            else if(res.status === 401){
+                history.push('/login')
+            }
+        })
         .then(data => setOrders(data))
     }, [])
     return (
